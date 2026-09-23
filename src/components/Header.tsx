7bfbcd864434a110d0e067245d82febebb4749d6
Yoonha Hwang <yoonha.highsummer@gmail.com>
2026-09-23 13:34:53 +0900

    Add blog on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { cvData } from '../data/cvData';
+import React from "react";
+import { cvData } from "../data/cvData";
 
 export const Header: React.FC = () => {
   const { profile } = cvData;
@@ -7,11 +7,15 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="profile-header">
-        <img src="/assets/portrait.png" alt="Portrait of Yoonha Hwang" className="profile-initials" />
+        <img
+          src="/assets/portrait.png"
+          alt="Portrait of Yoonha Hwang"
+          className="profile-initials"
+        />
         <div className="profile-info">
           <h1>{profile.name}</h1>
           <p className="tagline">
-            {profile.tagline.split(', ').map((part, index, array) => (
+            {profile.tagline.split(", ").map((part, index, array) => (
               <React.Fragment key={index}>
                 {part}
                 {index < array.length - 1 && (
@@ -27,7 +31,8 @@ export const Header: React.FC = () => {
       <div className="contact-info">
         {profile.email && (
           <a href={`mailto:${profile.email}`} className="contact-link">
-            <img src="/icons/email.png" alt="Email" className="contact-icon" /> {profile.email}
+            <img src="/icons/email.png" alt="Email" className="contact-icon" />{" "}
+            {profile.email}
           </a>
         )}
         {profile.phone && (
@@ -35,14 +40,25 @@ export const Header: React.FC = () => {
             <i className="fas fa-phone"></i> {profile.phone}
           </a>
         )}
-        {profile.github && (
-          <a href={`https://${profile.github}`} target="_blank" rel="noopener noreferrer" className="contact-link">
-            <img src="/icons/github.png" alt="GitHub" className="contact-icon" /> {profile.github}
+        {profile.blog && (
+          <a
+            href={`https://${profile.blog}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-link"
+          >
+            <img src="/icons/blog.png" alt="Blog" className="contact-icon" />{" "}
+            {profile.blog}
           </a>
         )}
         {profile.location && (
           <span className="contact-link">
-            <img src="/icons/earth.png" alt="Location" className="contact-icon" /> {profile.location}
+            <img
+              src="/icons/earth.png"
+              alt="Location"
+              className="contact-icon"
+            />{" "}
+            {profile.location}
           </span>
         )}
       </div>
